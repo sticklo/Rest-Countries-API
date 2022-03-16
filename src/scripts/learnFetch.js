338340356d3cec1fe15a516acdcb64af9c9e4a87
Sticklo <mafimisebiadebayo@gmail.com>
2022-03-16 13:13:34 +0100

    add error catch

--- src/scripts/learnFetch.js
+++ src/scripts/learnFetch.js
@@ -1,11 +1,17 @@
 const getCountries = async () => {
     const res = await fetch("https://restcountries.com/v2/all");
+
+    if (res.status !== 200){
+        throw new Error ('cannot fetch the data');
+    }
     const data = await res.json();
     return data;
 };
 
 
-getCountries().then(data => console.log('resolved:', data));
+getCountries()
+.then(data => console.log('resolved:', data))
+.catch(err => console.log('rejected:', err))
 
 
 // fetch()
