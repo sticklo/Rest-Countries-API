f308244db0dfea82b655861fc731e5ec543e8b0d
Sticklo <mafimisebiadebayo@gmail.com>
2022-03-18 14:24:35 +0100

    feat: implement dark-mode

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("data-theme", "light");
      // Get our button switcher
      var themeSwitcher = document.getElementById("theme-switcher");
 
      // When our button gets clicked
      themeSwitcher.onclick = function() {
+         console.log(this)
        // Get the current selected theme, on the first run
        // it should be `light`
        var currentTheme = document.documentElement.getAttribute("data-theme");
  
        // Switch between `dark` and `light`
        var switchToTheme = currentTheme === "dark" ? "light" : "dark"
- 
+       if (switchToTheme === "light"){
+           this.innerHTML = `<i class='bx bx-moon'></i><p>Dark mode</p>`
+
+       } else {
+        this.innerHTML = `<i class='bx bx-sun'></i><p>Light mode</p>`
+       }
        // Set our currenet theme to the new one
        document.documentElement.setAttribute("data-theme", switchToTheme);
      }
